cdef3bcbd8aa6e1af8484d0bfcee02d7b53387cf
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-11-03 02:34:47 -0600

    Create setup.py

---
+++ lib/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='x',
+    version='0.0.1',
+    author='x',
+    author_email='x',
+    description='x',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/x/x',
+    project_urls = {
+        "Bug Tracker": "https://github.com/x/x/issues"
+    },
+    license='x',
+    packages=['x'],
+    install_requires=['x'],
+)
