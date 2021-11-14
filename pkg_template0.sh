d389251ebbba7dbf67c5f479488836114e10269c
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2021-11-14 09:55:17 -0700

    Add files via upload

---
+++ pkg_template0.sh
@@ -0,0 +1,10 @@
+mkdir AwesomeApp
+touch AwesomeApp/__init__.py
+touch AwesomeApp/__main__.py
+touch AwesomeApp/awesome.py 
+touch AwesomeApp/readme.md
+touch AwesomeApp/requirements.txt
+touch AwesomeApp/some_module.py
+mkdir AwesomeApp/package
+touch AwesomeApp/package/__init__.py
+touch AwesomeApp/package/package_module.py
